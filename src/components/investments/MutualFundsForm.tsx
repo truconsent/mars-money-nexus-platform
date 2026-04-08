48101a072dc43d619e4a757e393a44d0977f5c7b
Deepak08-03 <deepakraja.ra@truconsent.io>
2026-04-08 21:23:32 +0530

    feat: integrate Google Tag Manager and add custom CMP SDK for consent management

--- src/components/investments/MutualFundsForm.tsx
+++ src/components/investments/MutualFundsForm.tsx
@@ -101,7 +101,7 @@ export const MutualFundsForm = ({ onBack }: MutualFundsFormProps) => {
           <TruConsentModal
             assetId={import.meta.env.VITE_TRU_CONSENT_ASSET_ID}
             userId={user ? user.id : guestId!}
-            logoUrl={"/lovable-uploads/d3d83a6e-8210-420a-a23b-0c89fc7ee3f4.png"}
+            // logoUrl={"/lovable-uploads/d3d83a6e-8210-420a-a23b-0c89fc7ee3f4.png"}
             bannerId={"CP010"}
             apiUrl={import.meta.env.VITE_TRU_CONSENT_API_URL}
             apiKey={import.meta.env.VITE_TRU_CONSENT_API_KEY}
